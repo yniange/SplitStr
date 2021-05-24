285199bf1a31c81dc60fdb5992429a01dd0c8c3e
yniange <40706122+yniange@users.noreply.github.com>
2021-05-25 07:50:04 +0800

    Add files via upload

---
+++ JP.cpp
@@ -0,0 +1,92 @@
+#include "JP.h"
+
+std::vector<std::string> JP::SplitStr(std::string sourcestr, char sep)
+{
+	std::vector<std::string> resultarray;//用来存储所有解析出来的子字符串
+	std::string strtmp;	//用来接收子字符串
+
+	//PrevC在遍历源字符串时记录下来当前字符，用来识别连续两个字
+	//符都是分隔字符的情况
+	char PrevC = '\0';
+	for (auto itc = sourcestr.begin(); itc < sourcestr.end(); ++itc)
+	{//itc迭代器，依次指向源字符串中的字符
+		if (sep == (*itc))//遇到分隔字符
+		{
+			if (itc == sourcestr.begin())
+			{
+				resultarray.push_back(" ");
+			}
+			else
+			{
+				if (PrevC != sep)//如果前一个字符不是分隔字符
+				{//则当前strtmp存储的是一个有效的字符串，将其压入resultarray
+					resultarray.push_back(strtmp);
+				}
+				else
+				{//如果前一个字符也是分隔字符，则应该往resultarray压入由一个空
+				 //格构成的字符串
+					resultarray.push_back(" ");
+				}
+			}
+			strtmp.clear();	//将strtmp清空，准备接收下一个子串的字符
+		}
+		else
+		{//当前字符不是分隔字符，则将其压入strtmp中
+			strtmp.push_back(*itc);
+		}
+		PrevC = (*itc);//记录下当前字符
+	}
+	if (strtmp.empty())//for循环结束后，如果strtmp中没有字符，则意味着
+	{//源字符串是以分隔字符串结尾的（不计空字符）,此时将strtmp置为空格
+	 //字符串，即在最后添加一个只有一个空格构成的字符串。
+		strtmp = " ";
+	}
+	resultarray.push_back(strtmp);	//将最后一个分隔字符后的子串压入resultarray
+
+	return resultarray;//将存储了所有子串的resultarray返回
+}
+
+//下标版本
+std::vector<std::string> JP::SplitString(std::string SourceStr, char sep)
+{
+	std::vector<std::string> ResultArray;//用来存储所有解析出来的子字符串
+	int len = static_cast<int>(SourceStr.size());
+	std::string strtmp = "";
+	char PrevC = '\0';
+	for (int i = 0; i < len; ++i)
+	{
+		if (SourceStr[i] == sep)
+		{
+			if (0 == i)
+			{
+				ResultArray.push_back(" ");
+			}
+			else
+			{
+				if (PrevC != sep)
+				{
+					ResultArray.push_back(strtmp);
+				}
+				else
+				{
+					ResultArray.push_back(" ");
+				}
+			}
+			strtmp.clear();
+		}
+		else
+		{
+			strtmp.push_back(SourceStr[i]);
+		}
+		PrevC = SourceStr[i];
+	}
+
+	if (strtmp.empty())//for循环结束后，如果strtmp中没有字符，则意味着
+	{//源字符串是以分隔字符串结尾的（不计空字符）,此时将strtmp置为空格
+	 //字符串，即在最后添加一个只有一个空格构成的字符串。
+		strtmp = " ";
+	}
+	ResultArray.push_back(strtmp);	//将最后一个分隔字符后的子串压入ResultArray
+
+	return ResultArray;//将存储了所有子串的ResultArray返回
+}
---
+++ JP.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//将一个字符串（string对象，形参1）以指
+//定字符为分隔符（默认为逗号','形参2），
+//切分为若干个字串，返回存放字串的容器。
+//如果源字符串中连续出现两个分隔符，则
+//认为这两个分隔符之间包含了一个" "子串
+//（即包含一个空格的字符串）。如果分隔
+//符在源字符串的开头或末尾，同样认为开
+//头或末尾处有一个" "子串。
+#include <vector>
+#include <string>
+
+class JP
+{
+public:
+	//迭代器版本
+	static std::vector<std::string> SplitStr(
+		std::string sourcestr, char sep = ',');
+	//下标版本
+	static std::vector<std::string> SplitString(
+		std::string SourceStr, char sep = ',');
+};
+
